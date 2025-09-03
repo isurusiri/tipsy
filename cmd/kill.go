be693eb20a2a0bfcd1fefafb58d3f80251ffffc9
Isuru Siriwardana <isuruanatomy@gmail.com>
2025-09-03 09:05:06 +0530

    kill feature

---
+++ cmd/kill.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/isurusiri/tipsy/internal/chaos"
+	"github.com/isurusiri/tipsy/internal/config"
+	"github.com/isurusiri/tipsy/internal/k8s"
+	"github.com/isurusiri/tipsy/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	selector  string
+	namespace string
+	count     int
+)
+
+// killCmd represents the kill command
+var killCmd = &cobra.Command{
+	Use:   "kill",
+	Short: "Kill pods based on label selector",
+	Long: `Kill pods in Kubernetes based on a label selector.
+
+This command will:
+1. List pods matching the provided label selector
+2. Randomly select the specified number of pods to kill
+3. Delete the selected pods (or simulate deletion in dry-run mode)
+
+Examples:
+  tipsy kill --selector "app=nginx" --count 2
+  tipsy kill --selector "environment=staging" --namespace production --count 1
+  tipsy kill --selector "tier=frontend" --dry-run --verbose`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Print configuration if verbose mode is enabled
+		PrintConfig()
+
+		// Validate required flags
+		if selector == "" {
+			utils.Error("--selector flag is required")
+			cmd.Help()
+			return
+		}
+
+		// Use global namespace if not specified locally
+		targetNamespace := namespace
+		if targetNamespace == "" {
+			targetNamespace = config.GlobalConfig.Namespace
+		}
+		if targetNamespace == "" {
+			targetNamespace = "default"
+		}
+
+		// Create Kubernetes client
+		client, err := k8s.NewClient(config.GlobalConfig.Kubeconfig)
+		if err != nil {
+			utils.Error(fmt.Sprintf("Failed to create Kubernetes client: %v", err))
+			return
+		}
+
+		// Execute the kill operation
+		err = chaos.KillPods(client, targetNamespace, selector, count, config.GlobalConfig.DryRun)
+		if err != nil {
+			utils.Error(fmt.Sprintf("Failed to kill pods: %v", err))
+			return
+		}
+
+		utils.Info("Kill operation completed successfully")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(killCmd)
+
+	// Local flags for the kill command
+	killCmd.Flags().StringVar(&selector, "selector", "", "Kubernetes label selector (required)")
+	killCmd.Flags().StringVar(&namespace, "namespace", "", "Kubernetes namespace to operate in (optional, defaults to global namespace or 'default')")
+	killCmd.Flags().IntVar(&count, "count", 1, "Number of pods to kill (optional, default 1)")
+
+	// Mark selector as required
+	killCmd.MarkFlagRequired("selector")
+}
---
+++ internal/chaos/kill.go
@@ -0,0 +1,75 @@
+package chaos
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/isurusiri/tipsy/internal/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// KillPods deletes pods based on a label selector
+func KillPods(client *kubernetes.Clientset, namespace, selector string, count int, dryRun bool) error {
+	utils.Info(fmt.Sprintf("Searching for pods with selector '%s' in namespace '%s'", selector, namespace))
+
+	// List pods matching the selector
+	pods, err := client.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: selector,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to list pods: %w", err)
+	}
+
+	if len(pods.Items) == 0 {
+		utils.Warn(fmt.Sprintf("No pods found matching selector '%s' in namespace '%s'", selector, namespace))
+		return nil
+	}
+
+	utils.Info(fmt.Sprintf("Found %d pod(s) matching selector", len(pods.Items)))
+
+	// Determine how many pods to kill
+	podsToKill := count
+	if len(pods.Items) < count {
+		podsToKill = len(pods.Items)
+		utils.Warn(fmt.Sprintf("Only %d pods available, limiting kill count to %d", len(pods.Items), podsToKill))
+	}
+
+	// Randomly select pods to kill
+	rand.Seed(time.Now().UnixNano())
+	selectedPods := make([]string, 0, podsToKill)
+	usedIndices := make(map[int]bool)
+
+	for len(selectedPods) < podsToKill {
+		index := rand.Intn(len(pods.Items))
+		if !usedIndices[index] {
+			usedIndices[index] = true
+			selectedPods = append(selectedPods, pods.Items[index].Name)
+		}
+	}
+
+	// Execute the kill operation
+	if dryRun {
+		utils.DryRun(fmt.Sprintf("Would delete %d pod(s):", len(selectedPods)))
+		for _, podName := range selectedPods {
+			utils.DryRun(fmt.Sprintf("  - %s", podName))
+		}
+	} else {
+		utils.Info(fmt.Sprintf("Deleting %d pod(s):", len(selectedPods)))
+		for _, podName := range selectedPods {
+			utils.Info(fmt.Sprintf("  Deleting pod: %s", podName))
+			
+			err := client.CoreV1().Pods(namespace).Delete(context.TODO(), podName, metav1.DeleteOptions{})
+			if err != nil {
+				utils.Error(fmt.Sprintf("Failed to delete pod '%s': %v", podName, err))
+				// Continue with other pods even if one fails
+			} else {
+				utils.Info(fmt.Sprintf("  Successfully deleted pod: %s", podName))
+			}
+		}
+	}
+
+	return nil
+}
